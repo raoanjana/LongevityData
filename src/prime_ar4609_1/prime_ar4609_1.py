1f3087e08d1276700d26c17cfca9bacaa8697b52
Anjana Rao <anjanarao@Anjanas-MacBook-Pro.local>
2023-11-01 13:57:02 -0400

    updated comment in method

--- src/prime_ar4609_1/prime_ar4609_1.py
+++ src/prime_ar4609_1/prime_ar4609_1.py
@@ -1,5 +1,22 @@
 import math
+"""
+    Returns if the integer is prime.
 
+    Parameters
+    ----------
+    n : integer
+ 
+    Returns
+    -------
+    Boolean
+      True or False, if the number is prime.
+
+    Examples
+    --------
+    >>> from prime_ar4609_1 import prime_ar4609_1
+    >>> prime_ar4609_1.is_prime(5)
+    True
+"""
 def is_prime(n):
     if n <= 1:
         return False
